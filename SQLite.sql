ac86cef8fe225ce73045bf753f6df42a56aaa607
DemePS <75910042+DemePS@users.noreply.github.com>
2025-08-28 13:00:15 +0200

    Some formatting

--- SQLite.sql
+++ SQLite.sql
@@ -1,5 +1,7 @@
 select sum(prix*qte) as CA from ventes;
+
 select produit, prix, sum(qte) as qte from ventes 
 group by produit;
-Select region, sum(qte*prix) as CA from ventes 
+
+Select region, sum(qte) as qte from ventes 
 Group by region;
